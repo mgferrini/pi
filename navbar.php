8a5e3790d74b374fcc7284d3a412e21518583050
Marcos Ferrini <marcos.ferrini@gmail.com>
2019-04-07 17:48:36 -0300

    Add files via upload
    navbar y footer de Ignacio

--- navbar.php
+++ navbar.php
@@ -1,52 +1,22 @@
-<head>
-  <link rel="stylesheet" href="css/nacho.css">
-</head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- <a class="navbar-brand" href="#">Logo</a> -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link home" href="?">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?page=login">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?page=registro">Registro</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Categorias</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item consejos" href="#">Consejos/Blog</a>
-            <a class="nav-link dropdown-toggle q_s" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Quienes Somos</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Nosotros</a>
-              <a class="dropdown-item" href="#">nuestra mision/filosofia/compromiso</a>
-              <a class="dropdown-item" href="#">Certificados Organicos</a>
-            </div>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle tienda" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Tienda</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Jabones</a>
-                <a class="dropdown-item" href="#">Aceites Hidratantes</a>
-                <a class="dropdown-item" href="#">Linea Capilar</a>
-              </div>
-            </li>
-          </li>
-          <div class="dropdown-divider"></div>
-      <li class="nav-item">
-        <a class="nav-link" href="?page=faq">Preguntas Frecuentes</a>
-      </li>
-    </ul>
-	<!-- <form class="form-inline my-2 my-lg-0" method="GET">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form> -->
-  </div>
+<section>
+  <article class="col-lg-6">
+    <a class="logo" href="index.html"><img src="img/logo-phi2.png" alt="logo" title="" style="width: 90.2854px; height: 87.2075px; position: relative;" class="imageResizerChangedClass" width="inherit" height="inherit"></a>
+    <h3 class="marca">Phi-Organic</h3>
+  </article>
+</section>
+<nav>
+<ul class="nav nav-pills">
+  <li class="nav-item"onloadeddata="">
+    <a class="nav-link soap" href="#">Jabones</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link aceite" href="#">Aceites</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link hidra" href="#">Hidratantes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link cap" href="#">Capilar</a>
+  </li>
+</ul>
 </nav>
