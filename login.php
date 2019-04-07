ee73b304a1584b02c3ff36e94e914f09570d5d48
Marcos Ferrini <marcos.ferrini@gmail.com>
2019-04-07 17:38:29 -0300

    Add files via upload
    actualizacion login y nuevo archivo reset

--- login.php
+++ login.php
@@ -1,42 +1,46 @@
 <div class="container-fluid regcontainer">
-    <section class="row">
-        <h1 class="regh1">Inicio de sesión</h1>
-        <br><br>
-    </section>
-    <section class="space">
-      <br>
-    </section>
-    <section class="registro row">
-      <div class="col-md-8 col-md-offset-4">
-        <article class="form regformulario col-xs-12 col-md-8 col-lg-8">
-      
-    		<div class="panel panel-default">
-			  	<div class="panel-heading">
-            <h2 class="regtitulo">Usuario y Contraseña</h2><br>
-          </div>
-          <div class="panel-body">
-            <form accept-charset="UTF-8" role="form">
-                    <fieldset>
-                <div class="form-group">
-                  <input class="form-control" placeholder="E-mail" name="email" type="text">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password" value="">
-              </div>
-              <div class="checkbox">
-                  <label>
-                    <input name="remember" type="checkbox" value="Remember Me"> Recordarme
-                  </label>
-                </div>
-              <input class="btn btn-lg btn-success btn-block" type="submit" value="Ingresar">
-            </fieldset>
-              </form>
-          </div>
-        </div>
-        
-        </article>   
-        
-      </div>
-    </section>
-	</div>
+	<section class="row">
+		<h1 class="regh1">Inicio de sesión</h1>
+		<br><br>
+	</section>
+	<section class="space">
+		<br>
+	</section>
+	<section class="registro row">
+		<div class="col-md-8 col-md-offset-4">
+			<article class="form regformulario col-xs-12 col-md-8 col-lg-8">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h2 class="regtitulo">Usuario y Contraseña</h2><br>
+					</div>
+					<div class="panel-body">
+						<form accept-charset="UTF-8" role="form">
+							<fieldset>
+								<div class="form-group">
+									<input class="form-control" placeholder="E-mail" name="email" type="text">
+								</div>
+								<div class="form-group">
+									<input class="form-control" placeholder="Password" name="password" type="password" value="">
+								</div>
+								<div class="checkbox">
+									<label>
+										<input name="remember" type="checkbox" value="Remember Me"> Recordarme
+									</label>
+								</div>
+								<input class="btn btn-lg btn-success btn-block" type="submit" value="Ingresar">
+								<hr>
+								<div class="forgot">
+									<a href="?page=reset">Olvidaste tu clave? hace click aca!</a>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+
+			</article>
+
+		</div>
+	</section>
+</div>
 </div>
---
+++ reset.php
@@ -0,0 +1,27 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 ">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<form>
+								<h3> <i class="fa fa-lock "></i> Reseteo de Clave</h3>
+								<p>Ingresa tu dirección de correo y recibiras un correo para generar una nueva clave.</p>
+								<div class="form-group">
+									<input type="email" name="email" id="email" class="form-control input-lg" placeholder="ingresa tu Email" tabindex="4">
+								</div>
+								<div class="row">
+									<div class="col-xs-12 col-md-12"><a href="#" class="btn btn-success btn-block btn-lg">Enviar</a></div>
+								</div>
+							</form>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</div>
