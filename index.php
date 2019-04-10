0db106ed1f9c260f1afe5fab1bbf934b0e66c7fe
Marsmin Maclon <nachogarcia003@gmail.comgit config --global user.name Ignacio-ggit config --global user.email nachogarcia003@gmail.com>
2019-04-10 01:58:20 -0300

    Index Responsive

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 	include_once("head.html");
+	echo "<div class='container-fluid'>";
 	include_once("header.php");
 	include_once("navbar.php");
-	
+	echo "<br>";
+	echo "<br>";
 	$queryString = $_GET;
-	
+
 	if( isset($queryString["page"])){
 		include_once( $queryString["page"]. ".php");
 	}else{
-		include_once("default.php");
+		include_once("carousel.php");
 	}
-	
+
 	include_once("footer.php");
 ?>
+</div>
