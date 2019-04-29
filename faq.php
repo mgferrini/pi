e7af9931dbffb2909e81102cd4e656370ae9d8aa
valelotartaro <48757573+valelotartaro@users.noreply.github.com>
2019-04-29 11:05:22 -0300

    Update faq.php
    cha chan

--- faq.php
+++ faq.php
@@ -1,43 +1,77 @@
+<link href="https://fonts.googleapis.com/css?family=Pinyon+Script" rel="stylesheet"> <!-- esto deberia ir en el head -->
 
-<div class="container-fluid regcontainer">
+<div class="container-fluid">
 	<section class="row">
 		<h1 class="regh1">
 			Preguntas Frecuentes
 		</h1>
 		<br><br>
 	</section>
-
+	<section class="row">
+	<article class="banner1">
+        <h5 class="titulocursiva">Phi Organic</h5>
+        <h3 class="tituloppal">Respeto desde el Origen</h3>
+    </article>   
+	<br><br>
+	</section>
 	<section class="registro row">
-		<article class="form regformulario col-xs-12 col-md-8 col-lg-8">
+		<article class="form col-xs-12 col-md-8 col-lg-8">
 			<div class="panel-group" id="faqAccordion">
 				<div class="panel panel-default ">
 					<div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question0">
 						<h4 class="panel-title">
-							<a href="#" class="ing regtitulo">P: Que colores de jabones existen?</a>
+							<a href="#question0" class="ing faqpreg">P: Que colores de jabones existen?</a>
 						</h4>
-
 					</div>
 					<div id="question0" class="panel-collapse collapse" style="height: 0px;">
 						<div class="panel-body">
-							<h5><span class="label label-primary">Respuesta</span></h5>
-
-							<p>Los jabones vienen de todos los colores</p>
+							<p class="faqresp">Los jabones vienen de todos los colores</p>
 						</div>
 					</div>
 				</div>
 				<div class="panel panel-default ">
 					<div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question1">
 						<h4 class="panel-title">
-							<a href="#" class="ing regtitulo">P: Que base tienen los aceites?</a>
+							<a href="#question1" class="ing faqpreg">P: Que base tienen los aceites?</a>
 						</h4>
-
 					</div>
 					<div id="question1" class="panel-collapse collapse" style="height: 0px;">
 						<div class="panel-body">
-							<h5><span class="label label-primary">Respuesta</span></h5>
-
-							<p>Los aceites tienen base organica.</p>
+							<p class="faqresp">Los aceites tienen base organica.</p>
 						</div>
+					</div>
+				</div>
+				<div class="panel panel-default ">
+					<div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question2">
+						<h4 class="panel-title">
+							<a href="#question2" class="ing faqpreg">P: Se puede pagar con tarjeta de cr&eacute;dito?</a>
+						</h4>
+
+					</div>
+					<div id="question2" class="panel-collapse collapse" style="height: 0px;">
+						<div class="panel-body">
+							<p class="faqresp">No tenemos habilitados los pagos con tajetas de cr&eacute;dito momentaneamente</p>
+						</div>
+					</div>
+				</div>
+				<div class="panel panel-default ">
+					<div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+						<h4 class="panel-title">
+							<a href="#question3" class="ing faqpreg">P: Tienen un showroom?</a>
+						</h4>
+					</div>
+					<div id="question3" class="panel-collapse collapse" style="height: 0px;">
+						<div class="panel-body">
+							<p class="faqresp">El showroom esta situado en Monroe 840, capital federal </p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		<article class="form col-xs-12 col-md-8 col-lg-8">
+	</section>
+	<!--/panel-group-->
+</div>
 					</div>
 				</div>
 				<div class="panel panel-default ">
