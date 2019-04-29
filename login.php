1913371fe40066edaf025b9477b2eb4a1958c500
Ignacio Garcia <48738658+Ignacio-G@users.noreply.github.com>
2019-04-29 17:40:52 -0300

    Update login.php

--- login.php
+++ login.php
@@ -47,6 +47,9 @@ function validarLogin($datos){
 }
 
 function abrirRegistro(){
+  if(!file_exists('usuarios.json')){
+    file_put_contents('usuarios.json',PHP_EOL, FILE_APPEND);
+  }
   $traer= file_get_contents("usuarios.json");//Traer el archivo
   $db = explode(PHP_EOL, $traer);//aca lo separas
   array_pop($db);//Sacas el ultimo elemento
@@ -70,25 +73,25 @@ function buscarDatos($usuarios, $email){// aca tenes que ir a buscar el usuario 
 ?>
 
 <?php
-	if(isset($errores)){
-		foreach ($errores as $value) {
-			echo $value;
-		}
-	}
+if(isset($errores)){
+  foreach ($errores as $value) {
+    echo $value;
+  }
+}
 ?>
 
 <div class="container regcontainer">
   <section class="row">
     <h1 class="regh1">Login</h1>
   </section>
-	<section class="registro row">
-  		<article class="form regformulario col-md-5">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h2 class="regtitulo">Usuario y Contraseña</h2><br>
-					</div>
-					<div class="panel-body">
-            <form accept-charset="UTF-8" role="form" method="post">
+  <section class="registro row">
+    <article class="form regformulario col-md-5">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h2 class="regtitulo">Usuario y Contraseña</h2><br>
+        </div>
+        <div class="panel-body">
+          <form accept-charset="UTF-8" role="form" method="post">
             <fieldset>
               <div class="form-group">
                 <input class="form-control" placeholder="E-mail" name="email" type="text" value="<?=isset($_COOKIE["email"])?$_COOKIE["email"] : "" ;?>">
@@ -96,26 +99,26 @@ function buscarDatos($usuarios, $email){// aca tenes que ir a buscar el usuario 
               <div class="form-group">
                 <input class="form-control" placeholder="Password" name="password" type="password" value="<?=isset($_COOKIE["password"])?$_COOKIE["password"] : "" ;?>">
               </div>
-								<div class="checkbox">
-									<label>
-										<input name="remember" type="checkbox" value="Remember Me"> Recordarme
-									</label>
-								</div>
-								<input class="btn regboton" type="submit" value="Ingresar">
-								<hr>
-								<div class="forgot">
-									<a href="?page=reset">Olvide mi contraseña</a>
-								</div>
-							</fieldset>
-						</form>
-					</div>
-				</div>
-			</article>
-      <article class="form regformulario col-md-5 offset-md-2">
-        <h2 class="regtitulo2">¿Acaso no estas registrado?</h2>
-        <p>Registrate para estar al tanto de las ultimas novedades acerca de los productos, los nuevos productos de Phi Organic, proximos lanzamientos y un monton de cosas mas.</p>
-        <br>
-        <a class="btn regboton" href="?page=registro">Registrarse</a>
-      </article>
-	</section>
+              <div class="checkbox">
+                <label>
+                  <input name="remember" type="checkbox" value="Remember Me"> Recordarme
+                </label>
+              </div>
+              <input class="btn regboton" type="submit" value="Ingresar">
+              <hr>
+              <div class="forgot">
+                <a href="?page=reset">Olvide mi contraseña</a>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </article>
+    <article class="form regformulario col-md-5 offset-md-2">
+      <h2 class="regtitulo2">¿Acaso no estas registrado?</h2>
+      <p>Registrate para estar al tanto de las ultimas novedades acerca de los productos, los nuevos productos de Phi Organic, proximos lanzamientos y un monton de cosas mas.</p>
+      <br>
+      <a class="btn regboton" href="?page=registro">Registrarse</a>
+    </article>
+  </section>
 </div>
