034eb88bf495cf53b45b01bef1fa1c865a5391e6
valelotartaro <48757573+valelotartaro@users.noreply.github.com>
2019-05-09 21:12:42 -0300

    Add files via upload

--- controllers/funciones.php
+++ controllers/funciones.php
@@ -1,13 +1,23 @@
 <?php
-
 session_start();
-require_once('usuarios.php');
 require_once("datos.php");
+require_once("./clases/Sesion.php");
+//Sesion::abrirSesion();
+require_once("helpers.php");
+require_once("./clases/Usuario.php");
 
+
+
+
+
+
+
+
+
+/* VALE: Esto ahora esta en helpers:
 function dd($valor){
 	echo "<pre>";
 	var_dump($valor);
 	echo "</pre>";
 	exit;
-}
-
+}*/
