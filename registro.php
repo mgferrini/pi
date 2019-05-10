3c661168e0ff8dab8ea922c72891c79d69416eaf
valelotartaro <48757573+valelotartaro@users.noreply.github.com>
2019-05-09 21:06:50 -0300

    Add files via upload

--- registro.php
+++ registro.php
@@ -1,19 +1,19 @@
 <?php
 include_once("controllers/funciones.php");
-//include_once("controllers/usuarios.php"); ESTO LO SAQUE PORQUE COPIE LAS FORMULAS MAS ABAJO
 if ($_POST){
-	$errores=validar($_POST,"registro");
+	$usuario = new Usuario ($_POST["email"],$_POST["password"],$_POST["nombre"],$_POST["apellido"],$_FILES["avatar"]["name"]);
+	//$usuario -> setTabla("./usuariosVale.json");  No funciona
+	$errores=$usuario->validarRegistro($usuario,$_POST["repassword"]);
 	if(count($errores)==0){
-		$avatar = armarAvatar($_FILES);
-		$registro = armarRegistro($_POST,$avatar);
-		guardar($registro);
-		seteoUsuario($registro,$_POST);
-		header("location: index.php");
+		$avatar = $usuario->armarAvatar($_FILES); //aca deberia mandar $usuario -> getAvatar() ??? porque no funciona
+		$registroUsuario= $usuario -> armarRegistro($usuario,$avatar);
+		$usuario -> guardar($registroUsuario) ;
+	//	Sesion::seteoUsuario($usuario,$_POST);      TODAVIA NO INTENTE HACERLO
+	redirect("index.php");
 	}
 }
-
 if (isset($_SESSION["nombre"])) {
-  header("location: index.php");
+	redirect("index.php");
 }
 ?>
 
@@ -127,139 +127,3 @@ if (isset($_SESSION["nombre"])) {
 </div>
 
 
-
-<?php
-
-function validar($datos,$bandera){
-	$errores=[];
-	if(isset($datos["nombre"])){
-		$nombre = trim($datos["nombre"]);
-		if(empty($nombre)){
-			$errores["nombre"]= "Debe introducir el nombre";
-		}
-	}
-	if(isset($datos["apellido"])){
-		$apellido = trim($datos["apellido"]);
-		if(empty($apellido)){
-			$errores["apellido"]= "Debe introducir el apellido";
-		}
-	}
-	if(!isset($datos["terminos"])){
-		$errores["terminos"]= "Debe aceptar Términos y Condiciones";
-	}
-
-	$email = trim($datos["email"]);
-	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-		$errores["email"]="Email invalido";
-	}
-	$password= trim($datos["password"]);
-	if(isset($datos["repassword"])){
-		$repassword = trim($datos["repassword"]);
-	}
-
-	if(empty($password)){
-		$errores["password"]= "Debe introducir contraseña";
-	}elseif (strlen($password)<6) {
-		$errores["password"]="La contraseña debe tener como mínimo 6 caracteres";
-	}
-	if(isset($datos["repassword"])){
-		if ($password != $repassword) {
-			$errores["repassword"]="Las contraseñas no coinciden";
-		}
-	}
-	if($bandera == "registro"){
-		if($_FILES["avatar"]["error"]!=0){
-			$errores["avatar"]="Error debe subir imagen";
-		}
-		$nombre = $_FILES["avatar"]["name"];
-		$ext = pathinfo($nombre,PATHINFO_EXTENSION);
-		if($ext != "png" && $ext != "jpg"){
-			$errores["avatar"]="Debe seleccionar archivo png ó jpg";
-		}
-	}
-	$usuario = buscarEmail($_POST["email"]);
-	if($usuario!= null){
-		$errores["email"]="Usuario ya existe";
-	}
-	return $errores;
-}
-
-function inputUsuario($campo){
-	if(isset($_POST[$campo])){
-		return $_POST[$campo];
-	}
-}
-
-function armarAvatar($imagen){
-	$nombre = $imagen["avatar"]["name"];
-	$ext = pathinfo($nombre,PATHINFO_EXTENSION);
-	$archivoOrigen = $imagen["avatar"]["tmp_name"];
-	$archivoDestino = pathinfo( dirname(__FILE__) )["dirname"].'/'.pathinfo( dirname(__FILE__) )["basename"] ;
-	$archivoDestino = $archivoDestino."/imagenes/";  //esto hay que ponerlo de acuerdo al directorio elegido
-	$avatar = uniqid();
-	$archivoDestino = $archivoDestino.$avatar;
-	$archivoDestino = $archivoDestino.".".$ext;
-	move_uploaded_file($archivoOrigen,$archivoDestino);
-	$avatar = $avatar.".".$ext;
-	return $avatar;
-}
-
-function armarRegistro($datos,$imagen){
-	$usuario = [
-	"nombre"=>$datos["nombre"],
-	"email"=>$datos["email"],
-	"password"=> password_hash($datos["password"],PASSWORD_DEFAULT),
-	"avatar"=>$imagen,
-	"perfil"=>1
-	];
-	return $usuario;
-}
-
-function guardar($usuario){
-	$jsusuario = json_encode($usuario);
-	file_put_contents('usuarios.json',$jsusuario. PHP_EOL, FILE_APPEND);
-}
-
-function buscarEmail($email){
-	$usuarios = abrirBaseDatos();
-	foreach ($usuarios as  $usuario) {
-		if($email === $usuario["email"]){
-			return $usuario;
-		}
-	}
-	return null;
-}
-
-
-function abrirBaseDatos(){
-	$baseDatosJson= file_get_contents("usuarios.json");
-	$baseDatosJson = explode(PHP_EOL,$baseDatosJson);
-	array_pop($baseDatosJson);
-	$arrayUsuarios=array();
-	foreach ($baseDatosJson as  $usuarios) {
-		$arrayUsuarios[]= json_decode($usuarios,true);
-	}
-	return $arrayUsuarios;
-}
-function seteoUsuario($user,$dato){
-	$_SESSION["nombre"]=$user["nombre"];
-	$_SESSION["email"] = $user["email"];
-	$_SESSION["perfil"]= $user["perfil"];
-	$_SESSION["avatar"]= $user["avatar"];
-	/* if(isset($dato["recordar"]) ){  LO SAQUE PARA REGISTRO
-	setcookie("email",$dato["email"],time()+3600);
-	setcookie("password",$dato["password"],time()+3600);
-	}*/
-}
-
-function validarUsuario(){
-	if($_SESSION["email"]){
-		return true;
-	}elseif ($_COOKIE["email"]) {
-		$_SESSION["email"]=$_COOKIE["email"];
-		return true;
-	}else{
-		return false;
-	}
-}
-?>
